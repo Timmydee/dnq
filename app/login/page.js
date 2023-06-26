fb452c1b66a93af19d1e78250e5bd03509256d5a
Akinola Emmanuel <akinolaemmanuel273@gmail.com>
2023-06-26 01:47:14 -0700

    footer edit

--- app/login/page.js
+++ app/login/page.js
@@ -138,15 +138,6 @@ const Login = () => {
         </div> */}
       </div>
 
-        <div className="w-[100%] h-[20px] lg:px-16 px-1 flex  lg:flex-row flex-col justify-center lg:justify-between items-center py-6 text-[#343A40] font-light lg:text-[16px] text-[14px] ">
-                 <div className="flex lg:flex-row flex-col lg:gap-6 gap-1 items-center ">
-                    <p>© 2023 Designers Query</p>
-                    <p>Terms and Conditions</p>
-                    <p>Privacy</p>
-                  </div>
-              
-                <p>English</p>
-        </div>
         
     </div>
   );
