ea05584f229b190cadf84ae228c916102ef9dec5
Akinola Emmanuel <akinolaemmanuel273@gmail.com>
2023-06-26 20:55:46 -0700

    edits

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -148,16 +148,7 @@ const SignUp = () => {
 
         <div className="lg:h-[90px] h-[150px]"></div>
 
-        <div className="w-[100%] h-[20px] lg:px-16 px-1 flex  lg:flex-row flex-col justify-center lg:justify-between items-center py-6 text-[#343A40] font-light lg:text-[16px] text-[14px] ">
-                 <div className="flex lg:flex-row flex-col lg:gap-6 gap-1 items-center ">
-                    <p>© 2023 Designers Query</p>
-                    <p>Terms and Conditions</p>
-                    <p>Privacy</p>
-                  </div>
-              <div>
-              <p>English</p>
-            </div>
-          </div>
+        
         {/* <div className="w-[50%] h-full hidden lg:block">
           <div className="w-[100%] h-[100%]">
             <Image src="/log.png" width={800} height={1200} alt="login" />
