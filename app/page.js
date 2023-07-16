5884d493def07fb1623ed6371862e3ad9384cfa3
Timmydee <dahunsitimmy@gmail.com>
2023-07-16 22:06:25 +0100

    dashboard

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import Frame1 from "@/homeComponent/Frame1";
 import Frame3 from "@/homeComponent/Frame3";
 import Frame5 from "@/homeComponent/Frame5";
-// import Testimonial from "@/homeComponent/Testimonial";
+import Testimonial from "@/homeComponent/Testimonial";
 import Unique from "@/homeComponent/unique";
 import Navbar from "@/components/Navbar";
 
@@ -16,9 +16,7 @@ export default function Home() {
         <Frame1 />
         <Frame3 />
         <Unique />
-        {/* <Testimonial /> */}
-        {/* <Flowbite /> */}
-        {/* <Swipe /> */}
+        <Testimonial />
         <Frame5 />
 
       </div>
--- homeComponent/Testimonial.jsx
+++ homeComponent/Testimonial.jsx
@@ -1,114 +1,55 @@
-// "use client";
-// import React, { useState } from "react";
-// import {FcPrevious, FcNext} from "react-icons/fc"
+"use client";
+import React, { useState } from "react";
+import {FcPrevious, FcNext} from "react-icons/fc"
 
-// const testimonies = [
-//   {
-//     imgUrl: "/swipe1.png",
-//     title: "Emily - UI/UX Designer.",
-//     desc:
-//       "Designers Query has been an invaluable resource for me. The community is incredibly supportive, and I've received insightful answers to my questions that have helped me grow as a designer. The platform is user-friendly and intuitive, making it easy to navigate and find the information I need. I highly recommend Designers Query to any designer looking to expand their skills and connect with like-minded professionals.",
-//   },
-//   {
-//     imgUrl: "/swipe2.png",
-//     title: "Alex - Experienced Designer",
-//     desc:
-//       "As a beginner designer,  Designers Query has been a game-changer for me. The platform has a welcoming and supportive community that has answered my questions and provided guidance every step of the way. The resources available, such as tutorials and articles, have helped me learn new techniques and improve my design skills. I feel more confident and inspired thanks to Designers Query.",
-//   },
-//   {
-//     imgUrl: "/swipe3.png",
-//     title: "Learning a skill in Web3.",
-//     desc:
-//       "Being a part of Designers Query has connected me with talented designers from around the world. I've had the opportunity to collaborate on projects, exchange ideas, and receive valuable feedback on my work. The platform provides a wealth of learning resources, tutorials, and expert articles that have helped me stay updated with the latest design trends. Designers Query is a must-have for any designer looking to enhance their skills and build meaningful relationships within the design community.",
-//   },
-//   {
-//     imgUrl: "/swipe3.png",
-//     title: "Esther - Graphic Designer",
-//     location:
-//       "Being a part of Designers Query has connected me with talented designers from around the world. I've had the opportunity to collaborate on projects, exchange ideas, and receive valuable feedback on my work. The platform provides a wealth of learning resources, tutorials, and expert articles that have helped me stay updated with the latest design trends. Designers Query is a must-have for any designer looking to enhance their skills and build meaningful relationships within the design community.",
-//   },
-//   {
-//     imgUrl: "/swipe3.png",
-//     title: "Learning a skill in Web3.",
-//     location:
-//       "Being a part of Designers Query has connected me with talented designers from around the world. I've had the opportunity to collaborate on projects, exchange ideas, and receive valuable feedback on my work. The platform provides a wealth of learning resources, tutorials, and expert articles that have helped me stay updated with the latest design trends. Designers Query is a must-have for any designer looking to enhance their skills and build meaningful relationships within the design community.",
-//   },
-//   {
-//     imgUrl: "/swipe3.png",
-//     title: "Esther - Graphic Designer",
-//     location:
-//       "Being a part of Designers Query has connected me with talented designers from around the world. I've had the opportunity to collaborate on projects, exchange ideas, and receive valuable feedback on my work. The platform provides a wealth of learning resources, tutorials, and expert articles that have helped me stay updated with the latest design trends. Designers Query is a must-have for any designer looking to enhance their skills and build meaningful relationships within the design community.",
-//   },
-// ];
+const testimonies = [
+  {
+    imgUrl: "/swipe1.png",
+    title: "Emily - UI/UX Designer.",
+    desc:
+      "Designers Query has been an invaluable resource for me. The community is incredibly supportive, and I've received insightful answers to my questions that have helped me grow as a designer. The platform is user-friendly and intuitive, making it easy to navigate and find the information I need. I highly recommend Designers Query to any designer looking to expand their skills and connect with like-minded professionals.",
+  },
+  {
+    imgUrl: "/swipe2.png",
+    title: "Alex - Experienced Designer",
+    desc:
+      "As a beginner designer,  Designers Query has been a game-changer for me. The platform has a welcoming and supportive community that has answered my questions and provided guidance every step of the way. The resources available, such as tutorials and articles, have helped me learn new techniques and improve my design skills. I feel more confident and inspired thanks to Designers Query.",
+  },
+  {
+    imgUrl: "/swipe3.png",
+    title: "Learning a skill in Web3.",
+    desc:
+      "Being a part of Designers Query has connected me with talented designers from around the world. I've had the opportunity to collaborate on projects, exchange ideas, and receive valuable feedback on my work. The platform provides a wealth of learning resources, tutorials, and expert articles that have helped me stay updated with the latest design trends. Designers Query is a must-have for any designer looking to enhance their skills and build meaningful relationships within the design community.",
+  },
 
-// const Testimonial = () => {
-//   const [slide, setSlide] = useState(1);
+];
 
-//   const handleClick = (index) => {
-//     setSlide(index)
-//   }
+const Testimonial = () => {
 
-//   const prevBtn = (prevIndex) => {
-//     setSlide(prevIndex === 0 ? testimonies.length - 1 : prevIndex - 1);
-//     console.log(add)
-//   };
+  return (
+    <div className="w-full my-10 lg:px-11 px-2">
+      <div className="lg:flex w-[100%] mx-auto justify-between overflow-hidden relative gap-4">
+        {testimonies.map((testimony, index) => (
+          <div className={`w-full flex flex-col items-center justify-center py-5 lg:h-[400px] h-fit`}>      
+              <div className={`flex items-center justify-center w-[56px] h-[56px] lg:w-[125px] lg:h-[125px] lg:absolute top-[0px] bottom-[0px] z-30`}>
+                <img src={testimony.imgUrl} fill alt={testimony.title} />
+              </div>
 
-//   // const nextBtn = () => {
-//   //   setSlide((prevIndex) => prevIndex === testimonies.length - 1 ? 0 : prevIndex + 1);
-//   //   console.log(sub)
-//   // };
-
-//   const nextBtn = () => {
-//     setSlide(slide === testimonies.length - 1 ? 0 : slide + 1)
-//   }
-
-
-//   console.log(slide)
-
-//   return (
-//     <div className="w-full ">
-//       <div className="lg:flex w-[100%] mx-auto justify-between overflow-hidden  ">
-//         {testimonies.map((testimony, index) => (
-//           <div className={` flex flex-col items-center ${
-//             index === slide ? 'w-[40%] h-[271px] relative bg-white rounded-[31.87px]' : ' w-[30%] h-[235px] pl-12 pr-[59.55px] pt-10 pb-[12px] bg-white rounded-3xl justify-start items-center inline-flex'
-//           }`}>
+              <div className={`flex flex-col items-center justify-center bg-white rounded-2xl shadow-lg shadow-blue-200 lg:w-[390px] px-3 lg:h-[185px] w-[90%] h-fit relative z-0 lg:pt-5`}>
+                <h4 className="text-blue-500 text-[16px] font-semibold mt-1 mb-2">{testimony.title}</h4>
+                <div className="w-[100%] h-[159px] text-zinc-800 text-[12px] font-light">
+                  <span>{testimony.desc} </span>
+                </div>
+              </div>
             
-//               <div className={`flex items-center justify-center ${index === slide ? 'w-[225px] h-[225px]' : 'w-[144px] h-[144px]'}`}>
-//                 <img src={testimony.imgUrl} fill alt={testimony.title} />
-//               </div>
+          </div>
+        ))}
+      </div>
 
-//               <div className={`flex flex-col items-center justify-center bg-white ${index === slide ? "shadow-2xl px-3" : "px-2 shadow-lg"}`}>
-//                 <h4 className="text-blue-500 text-[16px] font-semibold mt-1 mb-2">{testimony.title}</h4>
-//                 <div className="w-[100%] h-[159px] text-zinc-800 text-[12px] font-light">
-//                   <span>{testimony.desc} </span>
-//                 </div>
-//               </div>
-            
-//           </div>
-//         ))}
-//       </div>
-
-//       <div className="lg:flex justify-center mt-10 space-x-3 hidden ">
-//         <div className="w-[50px] h-[50px] border border-blue-500 rounded-full flex items-center justify-center">
-//         <button
-//           className=""
-//           onClick={prevBtn}
-//         >
-//           <FcPrevious />
-//         </button>
-//         </div>
-//         <div className="w-[50px] h-[50px] border border-blue-500 rounded-full flex items-center justify-center">
-//         <button
-//           className=""
-//           onClick={nextBtn}
-//         >
-//           <FcNext />
-//         </button>
-//         </div>
-//       </div>
+  
       
-//     </div>
-//   );
-// };
+    </div>
+  );
+};
 
-// export default Testimonial;
+export default Testimonial;
