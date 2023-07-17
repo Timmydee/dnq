da523560ad54cf249630f8b26158e1c75aa80d26
Timmydee <dahunsitimmy@gmail.com>
2023-07-17 17:44:07 +0100

    content change

--- homeComponent/Frame3.jsx
+++ homeComponent/Frame3.jsx
@@ -34,7 +34,7 @@ const Frame3 = () => {
   return (
     <div className='w-full bg-[#2E9EFF]'>
       <div className='py-4 lg:py-6 px-5 lg:px-0'>
-        <h3 className={`${styles.h5Style} text-center text-white font-bold font-pius`}>Helping all Designers get answers to there questions</h3>
+        <h3 className={`${styles.h5Style} text-center text-white font-bold font-pius`}>Helping all Designers get answers to their questions</h3>
         <div className='flex flex-wrap justify-evenly'>
           {help.map((item) => {
             return (
